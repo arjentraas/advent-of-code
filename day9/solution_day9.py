2148f1a67f90f6dee8f1ce22d7b03d12dd070388
Arjen Traas <arjen.traas@alliander.com>
2023-12-23 10:37:48 +0100

    day9

---
+++ day9/solution_day9.py
@@ -0,0 +1,56 @@
+from helper import read_input
+
+# from numpy import diff
+
+
+def part1():
+    inp = read_input("day9/input_day9.txt")
+    predictions = []
+    for line in inp:
+        history = [int(p) for p in line.split(" ")]
+        diffs = [[j - i for i, j in zip(history[:-1], history[1:])]]
+
+        while True:
+            diff = [j - i for i, j in zip(diffs[-1][:-1], diffs[-1][1:])]
+            if all(i == 0 for i in diff):
+                break
+            diffs.append(diff)
+
+        diffs.insert(0, history)
+        diffs.reverse()
+        for ind, nums in enumerate(diffs):
+            if ind == 0:
+                continue
+            nums[-1] = nums[-1] + diffs[ind - 1][-1]
+        predictions.append(nums[-1])
+    return sum(predictions)
+
+
+# print(f"Part 1: {part1()}")
+
+
+def part2():
+    inp = read_input("day9/input_day9.txt")
+    predictions = []
+    for line in inp:
+        history = [int(p) for p in line.split(" ")]
+        history.reverse()
+        diffs = [[i - j for i, j in zip(history[:-1], history[1:])]]
+        while True:
+            diff = [i - j for i, j in zip(diffs[-1][:-1], diffs[-1][1:])]
+            if all(i == 0 for i in diff):
+                break
+            diffs.append(diff)
+
+        diffs.reverse()
+        diffs.append(history)
+        for ind, nums in enumerate(diffs):
+            if ind == 0:
+                nums.append(nums[-1])
+                continue
+            nums.append(nums[-1] - diffs[ind - 1][-1])
+        predictions.append(nums[-1])
+    return sum(predictions)
+
+
+print(f"Part 2: {part2()}")
