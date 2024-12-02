f1ff2eeff0c0ed5010f6e42163f4ff408977fefb
Arjen <a.a.traas@gmail.com>
2024-12-02 18:23:13 +0100

    2024 day 1

---
+++ 2024/day1/day1.py
@@ -0,0 +1,45 @@
+from collections import Counter
+from pathlib import Path
+
+
+def part_1():
+    left_nums = []
+    right_nums = []
+    with Path("2024/day1/input_day1.txt").open("r") as handle:
+        for line in handle.readlines():
+            split = line.split(" ")
+            left_nums.append(int(split[0]))
+            right_nums.append(int(split[-1]))
+
+    total_diff = 0
+    for left, right in zip(sorted(left_nums), sorted(right_nums)):
+        total_diff += abs(left - right)
+
+    print(total_diff)
+
+
+part_1()
+
+
+def part_2():
+    left_nums = []
+    right_nums = []
+    with Path("2024/day1/input_day1.txt").open("r") as handle:
+        for line in handle.readlines():
+            split = line.split(" ")
+            left_nums.append(int(split[0]))
+            right_nums.append(int(split[-1]))
+
+    left_nums = Counter(left_nums)
+    right_nums = Counter(right_nums)
+
+    total_simularity = 0
+    for left in left_nums:
+        right_count = right_nums.get(left, 0)
+        simularity = left * right_count
+        total_simularity += simularity
+
+    print(total_simularity)
+
+
+part_2()
