cb3daab3867f95221e19dd9c048aa849c4a4983d
Arjen <a.a.traas@gmail.com>
2024-12-05 18:15:42 +0100

    day4 part 2

--- 2024/day4/day4.py
+++ 2024/day4/day4.py
@@ -35,7 +35,6 @@ def part_1():
     for offset in range(-max_x, max_x):
         diagonal = line_matrix.diagonal(offset)
         diagonal_string = "".join(diagonal)
-        print(diagonal)
         count += len(re.findall(straight, diagonal_string))
         count += len(re.findall(backwards, diagonal_string))
 
@@ -49,7 +48,6 @@ def part_1():
     for offset in range(-max_x, max_x):
         diagonal = inverse_line_matrix.diagonal(offset)
         diagonal_string = "".join(diagonal)
-        print(diagonal)
         count += len(re.findall(straight, diagonal_string))
         count += len(re.findall(backwards, diagonal_string))
     print(count)
@@ -59,7 +57,36 @@ part_1()
 
 
 def part_2():
-    pass
+    lines = read_input_lines("2024/day4/input_day4.txt")
+    count = 0
+
+    for i in range(1, len(lines) - 1):
+        line = lines[i]
+        for j in range(len(line) - 1):
+            char = line[j]
+            if char != "A":
+                continue
+
+            left_upper = lines[i - 1][j - 1]
+            right_upper = lines[i - 1][j + 1]
+            left_lower = lines[i + 1][j - 1]
+            right_lower = lines[i + 1][j + 1]
+            mas_count = 0
+            if left_upper == "M" and right_lower == "S":
+                mas_count += 1
+
+            if left_upper == "S" and right_lower == "M":
+                mas_count += 1
+
+            if right_upper == "M" and left_lower == "S":
+                mas_count += 1
+
+            if right_upper == "S" and left_lower == "M":
+                mas_count += 1
+
+            if mas_count == 2:
+                count += 1
+    print(count)
 
 
 part_2()
