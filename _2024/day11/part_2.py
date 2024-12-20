cfcfbc0a36291eb9cb8a86a9d5e1399ce2cd98ba
Arjen Traas <arjen.traas@alliander.com>
2024-12-20 17:58:47 +0100

    day11 part 2

--- _2024/day11/part_2.py
+++ _2024/day11/part_2.py
@@ -1,25 +1,38 @@
+from collections import defaultdict
+
 from helper import read_input
+
+
+def update(stone: int) -> list[int]:
+    if stone == 0:
+        return [1]
+    elif len(str(stone)) % 2 == 0:
+        return split_stone(stone)
+    return multiply(stone)
+
+
+def multiply(stone: int) -> list[int]:
+    return [stone * 2024]
+
+
+def split_stone(stone: int) -> list[int]:
+    halfway = int(len(str(stone)) / 2)
+    return [int(str(stone)[:halfway]), int(str(stone)[halfway:])]
 
 
 def main():
     input = read_input("_2024/day11/input.txt").strip()
-    stones = [int(char) for char in input.split(" ")]
+    stones = defaultdict(int, {int(char): 1 for char in input.split(" ")})
 
     for i in range(75):
-        new_stones = []
+        new_stones = defaultdict(int)
+        for stone, num in stones.items():
+            for new_stone in update(stone):
+                new_stones[new_stone] += num
 
-        for stone in stones:
-            if stone == 0:
-                new_stones.append(1)
-            elif len(str(stone)) % 2 == 0:
-                halfway = int(len(str(stone)) / 2)
-                new_stones.append(int(str(stone)[:halfway]))
-                new_stones.append(int(str(stone)[halfway:]))
-            else:
-                new_stones.append(stone * 2024)
         stones = new_stones
 
-    print(len(stones))
+    print(sum(stones.values()))
 
 
 main()
