57d3eacfe1feaa1943432d5661c74c7544e53806
Arjen <a.a.traas@gmail.com>
2024-12-21 12:57:49 +0100

    day 12 part 2 wip

---
+++ _2024/day12/part_2.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+
+import networkx as nx
+
+from helper import read_input_lines
+
+
+def main():
+    input = read_input_lines("_2024/day12/example_input.txt")
+    g = nx.Graph()
+    plant_types = set()
+
+    for y, line in enumerate(input):
+        for x, char in enumerate(line):
+            plant_types.add(char)
+            node_name = f"{x},{y}"
+            g.add_node(node_name, type=char)
+            if f"{x + 1},{y}" in g.nodes:
+                g.add_edge(node_name, f"{x + 1},{y}")
+            if f"{x - 1},{y}" in g.nodes:
+                g.add_edge(node_name, f"{x - 1},{y}")
+            if f"{x},{y+1}" in g.nodes:
+                g.add_edge(node_name, f"{x},{y+1}")
+            if f"{x},{y-1}" in g.nodes:
+                g.add_edge(node_name, f"{x},{y-1}")
+
+    total_price = 0
+    for plant_type in plant_types:
+        subgraph = g.copy()
+        subgraph.remove_nodes_from([n for n, n_data in subgraph.nodes(data=True) if n_data.get("type") != plant_type])
+        for island in nx.connected_components(subgraph):
+            area = len(island)
+            neighbour_counter = defaultdict(int)
+            for node in island:
+                neighbour_counter[len(subgraph.edges(node))] += 1
+
+    print(total_price)
+
+
+main()
