96a5cfe8c38e72315570cceed44f8ad8899c74fd
Arjen <a.a.traas@gmail.com>
2024-12-27 00:11:35 +0100

    day 12 part 2, finally!

--- _2024/day12/part_2.py
+++ _2024/day12/part_2.py
@@ -1,6 +1,25 @@
+from typing import Generator
+
 import networkx as nx
 
-from utils import STRAIGHT_DIRECTIONS, Grid, Position, read_input, read_input_lines
+from utils import (
+    DIAGONAL_DIRECTIONS,
+    DOWN,
+    HORIZONTAL_DIRECTIONS,
+    LEFT,
+    LEFT_LOWER,
+    LEFT_UPPER,
+    RIGHT,
+    RIGHT_LOWER,
+    RIGHT_UPPER,
+    STRAIGHT_DIRECTIONS,
+    UP,
+    VERTICAL_DIRECTIONS,
+    Grid,
+    Position,
+    read_input,
+    read_input_lines,
+)
 
 
 def get_graph(file):
@@ -22,8 +41,78 @@ def get_graph(file):
     return g
 
 
+INNER_CORNER_COMBOS = [
+    ((UP, LEFT), LEFT_UPPER),
+    ((UP, RIGHT), RIGHT_UPPER),
+    ((DOWN, RIGHT), RIGHT_LOWER),
+    ((DOWN, LEFT), LEFT_LOWER),
+]
+
+OUTER_CORNER_COMBOS = [(LEFT, UP), (LEFT, DOWN), (RIGHT, DOWN), (RIGHT, UP)]
+
+
+def get_corners(grid: Grid, island: Generator) -> int:
+    if len(list(island)) == 1:
+        return 4
+
+    if len(list(island)) == 2:
+        return 4
+
+    corners = 0
+    for node in island:
+        col, row = node
+        pos = Position(col, row)
+        straight_neighbors = set()
+        straight_neighbors_in_island = set()
+        straight_neighbors_outside = set()
+
+        for d in STRAIGHT_DIRECTIONS:
+            neighbor_pos = grid.get_neighbour_position(pos, d, False)
+            straight_neighbors.add(neighbor_pos)
+            if neighbor_pos and (neighbor_pos.col, neighbor_pos.row) in island:
+                straight_neighbors_in_island.add((neighbor_pos, d))
+            else:
+                straight_neighbors_outside.add((neighbor_pos, d))
+
+        horizontal_outside = {n[1] for n in straight_neighbors_outside if n[1] in HORIZONTAL_DIRECTIONS}
+        vertical_outside = {n[1] for n in straight_neighbors_outside if n[1] in VERTICAL_DIRECTIONS}
+
+        if len(straight_neighbors_outside) == 3 and len(straight_neighbors_in_island) == 1:
+            corners += 2
+            continue
+
+        if len(horizontal_outside) == 1 and len(vertical_outside) == 1:
+            corners += 1
+
+        if len(horizontal_outside) == 2 and len(vertical_outside) == 0:
+            continue
+
+        if len(horizontal_outside) == 0 and len(vertical_outside) == 2:
+            continue
+
+        diagonal_not_in_island = set()
+        for d in DIAGONAL_DIRECTIONS:
+            neighbor_pos = grid.get_neighbour_position(pos, d, False)
+            if neighbor_pos is None:
+                continue
+
+            if (neighbor_pos.col, neighbor_pos.row) not in island:
+                diagonal_not_in_island.add(d)
+
+        straight_neighbors_in_island_directions = {n[1] for n in straight_neighbors_in_island}
+        for straights, diagonal in INNER_CORNER_COMBOS:
+            if (
+                all(d in straight_neighbors_in_island_directions for d in straights)
+                and diagonal in diagonal_not_in_island
+            ):
+                corners += 1
+        pass
+
+    return corners
+
+
 def main():
-    file = "_2024/day12/example_input.txt"
+    file = "_2024/day12/input.txt"
     g = get_graph(file)
     grid = Grid(file)
     plant_types = set(read_input(file).strip())
@@ -37,17 +126,9 @@ def main():
         )
         for island in nx.connected_components(plant_graph):
             area = len(island)
-            perimeter = []
-            sides = set()
-            for node in island:
-                row, col = node
-                for direction in STRAIGHT_DIRECTIONS:
-                    neighbor = grid.get_neighbour_position(Position(row, col), direction)
-                    if neighbor and (neighbor.col, neighbor.row) in island:
-                        continue
-                    perimeter.append((node, (neighbor.col, neighbor.row)))
-            print(sides)
-            pass
+            corners = get_corners(grid, island)
+            island_price = area * corners
+            total_price += island_price
 
     print(total_price)
 
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from collections import UserDict
 from dataclasses import dataclass
 from functools import cache
 from pathlib import Path
@@ -16,8 +17,8 @@ def read_input(path: str):
 
 @dataclass(frozen=True)
 class Position:
-    col: int
-    row: int
+    col: float
+    row: float
 
 
 @dataclass(frozen=True)
@@ -28,22 +29,39 @@ class Direction:
     delta_col: int
 
 
-STRAIGHT_DIRECTIONS: list[Direction] = [
-    Direction(name="up", symbol="^", delta_row=-1, delta_col=0),
-    Direction(name="down", symbol="v", delta_row=1, delta_col=0),
-    Direction(name="right", symbol=">", delta_row=0, delta_col=1),
-    Direction(name="left", symbol="<", delta_row=0, delta_col=-1),
-]
+RIGHT = Direction(name="right", symbol=">", delta_row=0, delta_col=1)
+LEFT = Direction(name="left", symbol="<", delta_row=0, delta_col=-1)
+UP = Direction(name="up", symbol="^", delta_row=-1, delta_col=0)
+DOWN = Direction(name="down", symbol="v", delta_row=1, delta_col=0)
+
+LEFT_UPPER = Direction(name="left-upper", symbol="<^", delta_row=-1, delta_col=-1)
+LEFT_LOWER = Direction(name="left-lower", symbol="<v", delta_row=1, delta_col=-1)
+RIGHT_UPPER = Direction(name="right-upper", symbol=">^", delta_row=-1, delta_col=1)
+RIGHT_LOWER = Direction(name="right-lower", symbol=">v", delta_row=1, delta_col=1)
+
+STRAIGHT_DIRECTIONS: list[Direction] = [RIGHT, LEFT, DOWN, UP]
+HORIZONTAL_DIRECTIONS: list[Direction] = [RIGHT, LEFT]
+VERTICAL_DIRECTIONS: list[Direction] = [UP, DOWN]
+
+DIAGONAL_DIRECTIONS: list[Direction] = [LEFT_UPPER, LEFT_LOWER, RIGHT_UPPER, RIGHT_LOWER]
+
+ALL_DIRECTIONS = STRAIGHT_DIRECTIONS + DIAGONAL_DIRECTIONS
 
 
-class Grid(dict[Position, str]):
+class Grid(UserDict[Position, str]):
     max_row: int
     max_col: int
 
     def __hash__(self):
         return hash((self.max_row, self.max_col, frozenset(self.items())))
 
+    def __eq__(self, other):
+        if isinstance(other, Grid):
+            return self.max_row == other.max_row and self.max_col == other.max_col and dict(self) == dict(other)
+        return False
+
     def __init__(self, path: str):
+        super().__init__()
         self._from_file(path)
 
     def _from_file(self, path: str):
@@ -59,11 +77,18 @@ class Grid(dict[Position, str]):
         return 0 <= position.col <= self.max_col and 0 <= position.row <= self.max_row
 
     @cache
-    def get_neighbour_position(self, position: Position, direction: Direction) -> Position | None:
-        neighbor_position = Position(position.col + direction.delta_col, position.row + direction.delta_row)
-        if self._within_bounds(neighbor_position):
-            return neighbor_position
-        return
+    def get_neighbour_position(
+        self, position: Position, direction: Direction, within_bounds: bool = True, delta_factor: int = 1
+    ) -> Position | None:
+        neighbor_position = Position(
+            position.col + direction.delta_col * delta_factor, position.row + direction.delta_row * delta_factor
+        )
+        if within_bounds:
+            if self._within_bounds(neighbor_position):
+                return neighbor_position
+            else:
+                return None
+        return neighbor_position
 
     @cache
     def get_neighbour_value(self, position: Position, direction: Direction) -> str | None:
